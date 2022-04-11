1a0ac44cb96815727b429db645efc9af603f4139
Brian <bstanton773@gmail.com>
2022-04-11 12:01:00 -0500

    Convert App component to class component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from './components/Button';
 import Nav from "./components/Nav";
 
 
-function App(){
-    return (
-        <>
-            <Nav />
-            <div className='container'>
-                <h1>Hello World!</h1>
-                <Button step={1} />
-                <Button step={10} />
-                <Button step={100} />
-                <Button step={1000} />
+export default class App extends Component{
 
-            </div>
-        </>
-    )
+    render(){
+        return (
+            <>
+                <Nav />
+                <div className='container'>
+                    <h1>Hello World!</h1>
+                    <Button step={1} />
+                    <Button step={10} />
+                    <Button step={100} />
+                    <Button step={1000} />
+    
+                </div>
+            </>
+        )
+    }
 }
-
-export default App;
