b50af5cac1ffac488d80dd86b2548aee0fe7efb5
Brian <bstanton773@gmail.com>
2022-04-11 13:00:09 -0500

    Move count state to app

--- src/App.js
+++ src/App.js
@@ -4,17 +4,29 @@ import Nav from "./components/Nav";
 
 
 export default class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            count: 0
+        }
+    }
+
+    handleButtonClick = (step) => {
+        let newCount = this.state.count + step;
+        this.setState({
+            count: newCount
+        })
+    }
 
     render(){
+        const myButtonSteps = [1, 10, 100, 1000, 10000, 100000, 1000000]
         return (
             <>
                 <Nav />
                 <div className='container'>
                     <h1>Hello World!</h1>
-                    <Button step={1} />
-                    <Button step={10} />
-                    <Button step={100} />
-                    <Button step={1000} />
+                    <h4 className='text-center'>Current Count: {this.state.count}</h4>
+                    {myButtonSteps.map(step => <Button step={step} handleClick={this.handleButtonClick} />)}
     
                 </div>
             </>
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,24 +1,12 @@
 import React, { Component } from 'react'
 
 export default class Button extends Component {
-    constructor(props){
-        super(props);
-        console.log('Constructed')
-        this.state = {
-            count: 0
-        }
-    }
 
-    handleClick = () => {
-        console.log(`This button with a step of ${this.props.step} has been clicked!`)
-        let newCount = this.state.count + this.props.step;
-        this.setState({count: newCount})
-    }
 
     render() {
         console.log('Rendered')
         return (
-            <button onClick={this.handleClick} className='btn btn-primary d-block w-100'>Current: {this.state.count} Step: {this.props.step}</button>
+            <button onClick={() => this.props.handleClick(this.props.step)} className='btn btn-primary d-block w-100'>Step: {this.props.step}</button>
         )
     }
 }
