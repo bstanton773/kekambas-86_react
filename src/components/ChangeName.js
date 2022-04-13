655cba5bea4196e92b781cba49725ea31ecd1e28
Brian <bstanton773@gmail.com>
2022-04-13 10:46:45 -0500

    Update warning issue on unused var

--- src/components/ChangeName.js
+++ src/components/ChangeName.js
@@ -1,4 +1,5 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
+// import React, { Component } from 'react';
 
 // export default class ChangeName extends Component{
 //     constructor(props){
