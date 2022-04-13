1ea6cce80aec4e3d82a012dfd2d1fade903bec30
Brian <bstanton773@gmail.com>
2022-04-13 10:19:40 -0500

    Add useEffect to make request to API and set posts State with data

--- src/views/Posts.js
+++ src/views/Posts.js
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PostCard from '../components/PostCard';
 
 export default function Posts(props) {
-    const [posts, setPosts] = useState([1, 2, 3, 4, 5]);
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        fetch('https://kekambas-bs.herokuapp.com/posts')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setPosts(data)
+            })
+    }, [])
     return (
         <>
         <h1 className='text-center mt-3'>Posts</h1>
