494139adbd1c9c442f457dd71fbeac97947cb6fa
Brian <bstanton773@gmail.com>
2022-04-14 15:23:20 -0500

    Add post sorting to blog component

--- src/views/Blog.js
+++ src/views/Blog.js
@@ -3,6 +3,22 @@ import PostCard from '../components/PostCard';
 
 export default function Blog(props) {
     const [posts, setPosts] = useState([]);
+
+    const sortingMethods = {
+        byDateAsc: (a, b) => new Date(a.date_created) - new Date(b.date_created),
+        byDateDesc: (a, b) => new Date(b.date_created) - new Date(a.date_created),
+        byTitleAsc: (a, b) => a.title > b.title ? 1 : -1,
+        byTitleDesc: (a, b) => a.title > b.title ? -1 : 1
+    }
+
+    const sortPosts = (method) => {
+        console.log(method)
+        console.log(sortingMethods[method])
+        let sortedPosts = [...posts].sort(sortingMethods[method])
+        setPosts(sortedPosts)
+    }
+
+
     useEffect(() => {
         fetch('http://localhost:5000/api/posts')
             .then(res => res.json())
@@ -12,6 +28,17 @@ export default function Blog(props) {
         <>
             <h1 className='text-center'>Kekambas Blog</h1>
             <hr></hr>
+            <div className='row'>
+                <div className='offset-8 col-4'>
+                    <select onChange={(e)=>sortPosts(e.target.value)} className="form-select" aria-label="Default select example">
+                        <option>Sort Posts</option>
+                        <option value="byDateAsc">By Date Ascending</option>
+                        <option value="byDateDesc">By Date Descending</option>
+                        <option value="byTitleAsc">By Title Ascending</option>
+                        <option value="byTitleDesc">By Title Descending</option>
+                    </select>
+                </div>
+            </div>
             {posts.map(p => <PostCard post={p} key={p.id} />)}
         </>
     )
