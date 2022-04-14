45ab4582a0533fe9e73e2d886be882eb10884b2b
Brian <bstanton773@gmail.com>
2022-04-14 12:08:25 -0500

    Handle form submit and redirect to home on successful login

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react'
+import { Navigate } from 'react-router-dom';
 
 export default class Login extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            redirect: null
+        }
+    }
+
+    handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        let username = e.target.username.value;
+        let password = e.target.password.value;
+
+        let myHeaders = new Headers();
+        myHeaders.append('Authorization', "Basic " + btoa(`${username}:${password}`))
+
+        let res = await fetch('http://localhost:5000/api/token', {
+            headers: myHeaders
+        })
+        let data = await res.json();
+        if (data.error){
+            this.props.flashMessage('Your username/password is incorrect', 'danger')
+        } else{
+            let token = await data.token
+            localStorage.setItem('token', token)
+            this.props.flashMessage('You have successfully logged in', 'success')
+            this.setState({redirect: '/'})
+        }
+    }
+
     render() {
         return (
-            <form>
+            this.state.redirect ? <Navigate to={this.state.redirect} /> :
+            <form onSubmit={this.handleSubmit}>
                 <h3 className='text-center'>Login Here</h3>
                 <div className='form-group'>
                     <label htmlFor='username'>Username</label>
